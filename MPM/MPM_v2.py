9c83aea45762023396139a613072979b0ed0908c
Roy Rinberg <royrinberg@gmail.com>
2018-04-16 23:26:58 -0400

    cleaned up a bunch of nonsense that I wrote while debugging

--- MPM/MPM_v2.py
+++ MPM/MPM_v2.py
@@ -75,7 +75,7 @@ class MPM:
 				self.S[-1].append(a)
 			self.S[-1] = [item for sublist in self.S[-1] for item in sublist] # flatten S into 1 array
 		
-		self.I -=1
+		'''
 		print("I is "+ str(self.I))
 		print("u"+ str(len(self.S)))
 		for i in self.U:
@@ -86,43 +86,9 @@ class MPM:
 		for i in self.S:
 			print(i)
 			print(len(i))
-		
-		return
 		'''
-		def multilevel_decomposition_phase(self):
-		# the sequences S0, S1, ...SI(x) are formed
-		# each s_i consists of nonoverlapping substrings of x of length r^I-i
-		self.S.append([self.x])
-		self.U.append({self.x})
-		for i in range(1, self.I):
-			dic = {}
-			#self.U.append[{}]
-			size = (self.r**(self.I-i))
-			self.U.append({})
-			self.S.append([])
-			count =0
-			for ii in range(0, len(self.S[i-1])):
-				size = int(len(self.S[i-1][0])/self.r)
-				for rr in range(self.r):
-					a = self.S[i-1][ii][rr*(size):(rr+1)*(size)]
+		return
 
-					if( a not in self.U[i]):
-						self.U[i][a] = count
-						count+=1
-						self.S[i].append(a)
-						
-				#print(self.S[i])
-			#self.U.append(dic)
-			#self.S.append([self.x[ii : ii+ size] for ii in range(0,len(self.x), size) if self.x[ii : ii+ size] in self.U[i]  ] )
-			#for ii in range(len(U[i])):
-			#	S[i].append()
-#			#print(self.x[0:size])
-			#print([self.x[ii : ii+ size] for ii in range(0,len(self.x), size)])
-#			self.S.append([self.x[ii: ii+(self.r**(self.I-i)) ] for ii in range(0,len(self.x)),(self.r**(self.I-i))  ])
-		for i in self.S:
-			print(i)
-		return
-		'''
 
 	#produce Ts
 	def tokenization_phase(self):
@@ -160,18 +126,10 @@ class MPM:
 		#print(a)
 #		for i in range(1, len(self.T)):
 		print("length of T"+ str(len(self.T)))
-		for i in range(1, self.I+1): # needs to go to self.I+1	 (otherwise, doesnt' work
-			)	
+		for i in range(1, self.I): # needs to go to self.I+1	 (otherwise, doesnt' work)	
 			print("iteratopm"+ str(i))	
 			print("a is "+ str(a)+" prior")
-			'''
-			unique = 0
-			maxxer = -1
-			for k in range(len(self.T[i])):
-				if(self.T[i]>maxxer):
-					unique +=1
-					maxxer = self.T[i]
-			'''
+
 			#print("len a ", len(a), "\n")
 			for j in range(len(a)):
 				#print("from ", self.r*a[j], " to ", (1+a[j])*self.r)
@@ -186,27 +144,7 @@ class MPM:
 			print("level i", str(i), a)
 		return a
 #		print(a)
-"""
-	def encoding_phase(self):
-		a = self.T[0]
-		print(a)
-		for i in range(1,len(self.T)-1):# for each T
-			count = 0
-			tempdict = {}
-			for j in range(len(a)): # do the CFG decryption 
-				temp = []
-				if(a[j] not in tempdict):
-					tempdict[a[j]] = count
-					for q in range(self.r):
-						temp.append(self.T[i][(self.r*count)+q ])
-					count+=1
 
-				a[j] = temp
-			a = [item for sublist in a for item in sublist] # flatten a
-			print("level i", str(i), a)
-		#print("AAHAHAHAHAHHAHHA", a)
-		#print(a)
-"""
 if __name__ == '__main__':
 	s = "00100100100000010010000010010010"
 	print(sys.argv)
@@ -240,30 +178,4 @@ if __name__ == '__main__':
 	'''
 
 
-
-
-
-
-'''
-T's
-['00100100', '10000001', '00100000', '10010010']
-[0, 1, 2, 3]
-['0010', '0100', '1000', '0001', '0010', '0000', '1001', '0010']
-[0, 1, 2, 3, 0, 4, 5, 0]
-['00', '10', '01', '00', '10', '00', '00', '01', '00', '00', '10', '01']
-[0, 1, 2, 0, 1, 0, 0, 2, 0, 0, 1, 2]
-['0', '0', '1', '0', '0', '1']
-[0, 0, 1, 0, 0, 1]
-
-['0010010010000001', '0010000010010010']
-[0, 1]
-['00100100', '10000001', '00100000', '10010010']
-[0, 1, 2, 3]
-['0010', '0100', '1000', '0001', '0010', '0000', '1001', '0010']
-[0, 1, 2, 3, 0, 4, 5, 0]
-['00', '10', '01', '00', '10', '00', '00', '01', '00', '00', '10', '01']
-[0, 1, 2, 0, 1, 0, 0, 2, 0, 0, 1, 2]
-['0', '0', '1', '0', '0', '1']
-[0, 0, 1, 0, 0, 1]
-'''
 
